715bee3838d7718df4e25487241982e60821d508
Max Veytsman <maxim@ontoillogical.com>
2020-06-27 14:12:46 -0400

    nix-shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with (import <nixpkgs> { });
+mkShell {
+  name = "mc-hammer";
+  buildInputs = [elixir_1_10];
+  shellHook = ''
+    mkdir -p .nix-mix
+    mkdir -p .nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-hex
+    export PATH=$MIX_HOME/bin:$PATH
+    export PATH=$HEX_HOME/bin:$PATH
+    export ERL_AFLAGS="-kernel shell_history enabled"
+  '';
+}
